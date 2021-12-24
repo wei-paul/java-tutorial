856252ffaa5d8481f8ebc7ecd975317b97e1458f
Paul <paul.wei607@gmail.com>
2021-12-23 18:46:31 -0800

    difference between primitive/reference

--- Reference.java
+++ Reference.java
@@ -1,10 +1,14 @@
 import java.util.Date;
-
+import java.awt.*;
 public class Reference {
   public static void main(String[] args) {
     //Concept: When dealing with reference types (complex objects) you need to allocate memory (using the new)
     Date now = new Date();
     System.out.println(now);
-    
+    //Concet: Difference between storing primitive type data and reference type data is that the reference type will store the ADDRESS of the memory. Compared to the VALUE (primitive type).
+    Point point1 = new Point(1, 1);
+    Point point2 = point1;
+    point1.x = 2;
+    System.out.println(point2);
   }
 }
