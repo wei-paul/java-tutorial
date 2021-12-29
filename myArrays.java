510e3435bdfb6ea67247f87616e3e9b4d05860fd
Paul <paul.wei607@gmail.com>
2021-12-28 17:20:55 -0800

    how to array

---
+++ myArrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class myArrays {
+  
+  public static void main(String[] args) {
+    int[] numbers = { 2, 3, 5, 1, 4 };
+    Arrays.sort(numbers);
+    System.out.println(Arrays.toString(numbers));
+  }
+}
