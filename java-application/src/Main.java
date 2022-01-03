049708454a52d280256c0ebeaf35774a81b0244b
Paul <paul.wei607@gmail.com>
2022-01-02 21:21:04 -0800

    finishes simple mortgage calculator

--- java-application/src/Main.java
+++ java-application/src/Main.java
@@ -5,23 +5,25 @@ public class Main {
   public static void main(String[] args) {
     final byte MONTHS_IN_YEAR = 12;
     final byte PERCENT = 100;
+    Scanner scanner = new Scanner(System.in);
+    
     System.out.print("Principal: ");
-    Scanner principalScanner = new Scanner(System.in);
-    int principal = principalScanner.nextInt();
+    int principal = scanner.nextInt();
         
     System.out.print("Annual Interest Rate: ");
-    Scanner interestRateScanner = new Scanner(System.in);
-    double interestRate = interestRateScanner.nextDouble();
+    float annualInterest = scanner.nextFloat();
+    float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
     System.out.print("Period (Years): ");
-    Scanner periodScanner = new Scanner(System.in);
-    byte period = periodScanner.nextByte();
+    byte period = scanner.nextByte();
+    int numberOfPayments = period * MONTHS_IN_YEAR;
  
-    // M = P ( r (1 + r)^n / (1 + r)^n - 1 )
-    NumberFormat currency = NumberFormat.getCurrencyInstance();
-    // String result = currency.format(//);
+    double mortgage = principal
+                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                    / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
     
-    System.out.print("Annual Interest Rate: ");
+    System.out.print("Mortgage: " + mortgageFormatted);
     
   }
 }
